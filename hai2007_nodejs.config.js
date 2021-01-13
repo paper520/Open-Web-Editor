a2af7678ad3d6378287c13226cf4d62cd7356466
你好2007 <2501482523@qq.com>
2021-01-13 17:03:39 +0800

    cuf -> hai2007_nodejs

--- hai2007_nodejs.config.js
+++ hai2007_nodejs.config.js
@@ -34,22 +34,22 @@ module.exports = {
     // 定义任务
     task: {
 
-        init(cuf, pkg) {
+        init(hai2007_nodejs, pkg) {
 
-            cuf.print(pkg.name + "@" + pkg.version + " " + pkg.description);
+            hai2007_nodejs.print(pkg.name + "@" + pkg.version + " " + pkg.description);
 
             // 如果打包后的文件存在
-            if (fs.existsSync('./dist')) cuf.deleteSync('./dist');
+            if (fs.existsSync('./dist')) hai2007_nodejs.deleteSync('./dist');
 
-            cuf.log("\n-----------------------\n环境整理完毕，开始打包！\n-----------------------");
-            cuf.print("Date : " + new Date() + "\n");
+            hai2007_nodejs.log("\n-----------------------\n环境整理完毕，开始打包！\n-----------------------");
+            hai2007_nodejs.print("Date : " + new Date() + "\n");
 
         },
 
-        end(cuf) {
+        end(hai2007_nodejs) {
 
-            cuf.log("\n-----------------------\n打包完毕！\n-----------------------");
-            cuf.print("Date : " + new Date() + "\n\n");
+            hai2007_nodejs.log("\n-----------------------\n打包完毕！\n-----------------------");
+            hai2007_nodejs.print("Date : " + new Date() + "\n\n");
 
             // 打印文件大小
             const printFileSize = function (index, url) {
@@ -65,7 +65,7 @@ module.exports = {
                             else return (bytes / 1073741824).toFixed(3) + " GB";
                         })(stats.size);
 
-                        cuf.log("[" + index + "]    " + url + "    " + size);
+                        hai2007_nodejs.log("[" + index + "]    " + url + "    " + size);
                     }
 
                 });
@@ -80,15 +80,15 @@ module.exports = {
          * 第一步：模块打包
          * ----------------------
          */
-        bundle(cuf) {
+        bundle(hai2007_nodejs) {
             async function build(inputOptions, outputOptions) {
                 const bundle = await rollup.rollup(inputOptions);
                 await bundle.write(outputOptions);
 
-                cuf.error('模块打包完毕');
+                hai2007_nodejs.error('模块打包完毕');
             }
 
-            cuf.log("\n[1]模块打包:./dist/module.new.js\n");
+            hai2007_nodejs.log("\n[1]模块打包:./dist/module.new.js\n");
 
             build({
                 input: 'src/index.js',
@@ -119,17 +119,17 @@ module.exports = {
          * 第二步：babel转义
          * ----------------------
          */
-        babel(cuf, pkg) {
+        babel(hai2007_nodejs, pkg) {
 
-            cuf.log("\n[2]babel转义:./dist/module.new.js → ./dist/open-web-editor.js\n");
+            hai2007_nodejs.log("\n[2]babel转义:./dist/module.new.js → ./dist/open-web-editor.js\n");
 
             babel.transformFile("./dist/module.new.js", {}, function (err, result) {
                 if (!err) {
                     fs.writeFileSync("./dist/open-web-editor.js", banner(pkg));
                     fs.appendFileSync("./dist/open-web-editor.js", result.code);
-                    cuf.deleteSync("./dist/module.new.js");
+                    hai2007_nodejs.deleteSync("./dist/module.new.js");
 
-                    cuf.error('转义完毕');
+                    hai2007_nodejs.error('转义完毕');
                 } else {
                     console.log(err);
                 }
@@ -140,9 +140,9 @@ module.exports = {
          * 第三步：压缩代码
          * ----------------------
          */
-        uglifyjs(cuf, pkg) {
+        uglifyjs(hai2007_nodejs, pkg) {
 
-            cuf.log("\n[3]压缩代码:./dist/open-web-editor.js → ./dist/open-web-editor.min.js\n");
+            hai2007_nodejs.log("\n[3]压缩代码:./dist/open-web-editor.js → ./dist/open-web-editor.min.js\n");
 
             cp.exec("uglifyjs ./dist/open-web-editor.js -m -o ./dist/uglifyjs.new.js", function (error) {
                 if (error) {
@@ -152,9 +152,9 @@ module.exports = {
                     fs.writeFileSync("./dist/open-web-editor.min.js", banner(pkg));
                     fs.appendFileSync("./dist/open-web-editor.min.js", fs.readFileSync("./dist/uglifyjs.new.js"));
 
-                    cuf.error('压缩完毕');
+                    hai2007_nodejs.error('压缩完毕');
                 }
-                cuf.deleteSync("./dist/uglifyjs.new.js");
+                hai2007_nodejs.deleteSync("./dist/uglifyjs.new.js");
             });
         }
 
